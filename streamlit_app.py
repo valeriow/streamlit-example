00c50cf923c036512d944b1b96a373d8bdd49f16
valeriow <valeriow+github@gmail.com>
2021-10-15 00:01:49 -0300

    melhorias

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,16 @@ import streamlit as st
 
 
 
+def get_table_download_link2(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+    return href
+
 
 df = pd.read_csv("base_nit2.csv")
 features = ['transaction_sale','features_suite','features_bedroom','total_area','features_garage','features_bathroom','sqrmeter_price_area_sale','harvesine_distance']
@@ -42,6 +52,27 @@ df3 = df.iloc[selec]
 
  O primeiro da lista é a amostra selecionada
 """
-df3[['transaction_sale','features_suite','features_bedroom','total_area','features_garage','features_bathroom','sqrmeter_price_area_sale','harvesine_distance', 'original_address_neighborhood','state_name','city_raw']]
+df4 = df3[['transaction_sale','features_suite','features_bedroom','total_area','features_garage','features_bathroom','sqrmeter_price_area_sale','harvesine_distance', 'original_address_neighborhood','state_name','city_raw']]
+df4
 
-       
+from io import BytesIO
+import base64
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    val = to_excel(df)
+    b64 = base64.b64encode(val)  
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download xls </a>' 
+
+st.markdown(get_table_download_link(df4), unsafe_allow_html=True)
+
